b7c6cdf3c31ab5d8428ef5bfd5af50b4a1a93d2e
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2024-10-29 23:46:02 +0300

    Добавлена поддержка UUIDv7

---
+++ TgBotFrame.Data/UUIDv7.cs
@@ -0,0 +1,63 @@
+﻿using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace TgBotFrame.Data;
+
+public static class UUIDv7
+{
+    public static readonly Guid Max = new([
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+        255,
+    ]);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Guid NewUUIDv7Fast()
+    {
+        Span<byte> result = stackalloc byte[16];
+
+        BinaryPrimitives.WriteInt64BigEndian(result, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() << 16);
+
+        Span<byte> randomSpan = result[6..];
+        Random.Shared.NextBytes(randomSpan);
+
+        result[6] &= 0b0000_1111;
+        result[6] |= 0b0111_0000;
+        result[8] &= 0b0011_1111;
+        result[8] |= 0b1000_0000;
+
+        return new(result, true);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Guid NewUUIDv7()
+    {
+        Span<byte> result = stackalloc byte[16];
+
+        BinaryPrimitives.WriteInt64BigEndian(result, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() << 16);
+
+        Span<byte> randomSpan = result[6..];
+        RandomNumberGenerator.Fill(randomSpan);
+
+        result[6] &= 0b0000_1111;
+        result[6] |= 0b0111_0000;
+        result[8] &= 0b0011_1111;
+        result[8] |= 0b1000_0000;
+
+        return new(result, true);
+    }
+}
