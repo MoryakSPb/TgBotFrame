fa1c5b614f073a8a833ad94d9407d1320e1caf7b
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2024-10-30 05:41:45 +0300

    Форматирование кода

--- TgBotFrame.Commands/Middleware/CommandRouterMiddleware.cs
+++ TgBotFrame.Commands/Middleware/CommandRouterMiddleware.cs
@@ -115,9 +115,9 @@ public sealed class CommandRouterMiddleware(
             + Environment.NewLine
             + string.Format(Resources.ResourceManager.GetString(@"UseHelpMessage", Culture)!,
                 ExceptedName), replyParameters: new()
-            {
-                MessageId = update.Message.MessageId,
-            },
+                {
+                    MessageId = update.Message.MessageId,
+                },
             cancellationToken: ct).ConfigureAwait(false);
     }
 
