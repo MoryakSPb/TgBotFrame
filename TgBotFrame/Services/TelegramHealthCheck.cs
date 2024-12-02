2439be7c83089bde9a1ec8c7fd44d80bf67650df
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2024-12-02 08:18:28 +0300

    Добавлен TelegramHealthCheck; обновлены версии пакетов

---
+++ TgBotFrame/Services/TelegramHealthCheck.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Telegram.Bot;
+
+namespace TgBotFrame.Services;
+
+public sealed class TelegramHealthCheck(ITelegramBotClient botClient) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+        => await botClient.TestApi(cancellationToken).ConfigureAwait(false)
+            ? HealthCheckResult.Healthy()
+            : HealthCheckResult.Unhealthy();
+}
