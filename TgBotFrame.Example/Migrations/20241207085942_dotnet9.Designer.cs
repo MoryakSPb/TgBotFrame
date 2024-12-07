458254324dc017a4a41951240d711c8ed8c2e67b
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2024-12-07 12:00:24 +0300

    Обновлена версия .NET в миграциях

--- TgBotFrame.Example/Migrations/20241207085942_dotnet9.Designer.cs
+++ TgBotFrame.Example/Migrations/20241207085942_dotnet9.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TgBotFrame.Example;
 
@@ -10,9 +11,11 @@ using TgBotFrame.Example;
 namespace TgBotFrame.Example.Migrations
 {
     [DbContext(typeof(ExampleDataContext))]
-    partial class ExampleDataContextModelSnapshot : ModelSnapshot
+    [Migration("20241207085942_dotnet9")]
+    partial class dotnet9
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
---
+++ TgBotFrame.Example/Migrations/20241207085942_dotnet9.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TgBotFrame.Example.Migrations
+{
+    /// <inheritdoc />
+    public partial class dotnet9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
