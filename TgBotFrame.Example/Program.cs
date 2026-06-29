467dc43f8ac241bb9b3fcd4c6c557bb833e8e870
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2026-06-29 19:43:11 +0300

    Реформат кода

--- TgBotFrame.Example/Program.cs
+++ TgBotFrame.Example/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddTelegramHttpClient();
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(provider =>
 {
     IHttpClientFactory factory = provider.GetRequiredService<IHttpClientFactory>();
-   return new(tgToken!, factory.CreateClient(nameof(ITelegramBotClient)));
+    return new(tgToken!, factory.CreateClient(nameof(ITelegramBotClient)));
 });
 
 builder.Services.AddDbContext<ExampleDataContext>(optionsBuilder =>
